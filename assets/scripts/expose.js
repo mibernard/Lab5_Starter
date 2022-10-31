7803c73dc458c8228f7397a7a3b93afcd5e18c69
mibernard <83428436+mibernard@users.noreply.github.com>
2022-10-30 23:19:24 -0700

    adding volume slider image functionality

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -30,6 +30,20 @@ function init() {
     document.querySelector('#expose img').src = hornSource;
     document.querySelector('#expose audio').src = audioSource;
   })
+  
+  //event listener for changing the volume slider image
+  volumeSlider.addEventListener("input", (event) => {
+    //let volumeValue = volumeSlider.getAttribute("value");
+    let volValue = event.target.value;
+
+    if (volValue > 66)
+      volSource.setAttribute("src", "assets/icons/volume-level-3.svg");
+    else if (volValue > 33)
+      volSource.setAttribute("src", "assets/icons/volume-level-2.svg");
+    else if (volValue > 0)
+      volSource.setAttribute("src", "assets/icons/volume-level-1.svg");
+    else volSource.setAttribute("src", "assets/icons/volume-level-0.svg");
+  });
 
   // Add event listener for when the play sound button is pressed
   const soundButton = document.querySelector('#expose button');
