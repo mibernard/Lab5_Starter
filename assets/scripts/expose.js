b0d5ce9265ec7b593fd4efb18545037afff97cff
Matthew <matthewbernard4@gmail.com>
2022-10-30 22:46:29 -0700

    volume slider funcitonality

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,41 +3,6 @@
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
-  // TODO
-
-  // Add event listener for when the horn selector element changes
-  const hornSelector = document.getElementById("horn-select");
-  hornSelector.addEventListener("change", (event) => {
-    // Current value of horn selector
-    const hornType = event.target.value;
-    // Default image and audio sources
-    let hornSource = "assets/images/no-image.png";
-    let audioSource = "";
-    // Change image and audio sources based on selector
-    switch (hornType) {
-      case "air-horn":
-        hornSource = "assets/images/air-horn.svg";
-        audioSource = "assets/audio/air-horn.mp3";
-        break;
-      case "car-horn":
-        hornSource = "assets/images/car-horn.svg";
-        audioSource = "assets/audio/car-horn.mp3";
-        break;
-      case "party-horn":
-        hornSource = "assets/images/party-horn.svg";
-        audioSource = "assets/audio/party-horn.mp3";
-        break;
-    }
-    // Update image and audio elements with new sources
-    document.querySelector("#expose img").src = hornSource;
-    document.querySelector("#expose audio").src = audioSource;
-  });
-
-  let volumeSlider = document.getElementById("volume");
-  let volSource = document.querySelector(
-    "[src = 'assets/icons/volume-level-2.svg']"
-  );
-
   volumeSlider.addEventListener("input", (event) => {
     //let volumeValue = volumeSlider.getAttribute("value");
     let volValue = event.target.value;
